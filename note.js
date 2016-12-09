0e6d2382fb33b62fb9ecceb75263ea5d830411ed
rxon <rxon@users.noreply.github.com>
2016-12-10 00:49:39 +0900

    Update note.js 非同期処理まだ

--- note.js
+++ note.js
@@ -3,53 +3,82 @@
 const fs = require('fs');
 const path = require('path');
 
-const ejs = require('ejs');
 const express = require('express');
 const hljs = require('highlight.js');
+const _ = require('lodash');
 const marked = require('marked');
-const metaMarked = require('meta-marked');
+const mustache = require('mustache');
 
 const app = express();
 const mdDir = path.join(__dirname, '/post/');
 const viewsDir = path.join(__dirname, '/views/');
 
-app.engine('.html', ejs.__express);
+app.engine('mustache', mustache);
 app.set('views', viewsDir);
-app.set('view engine', 'html');
+app.set('view engine', 'mustache');
 
-function readMd(req, res, next) {
-  fs.readdir(mdDir, (err, files) => {
-    if (err) {
-      throw new Error('Failed to read md dir');
-    }
-    files.forEach(file => {
-
-      const mdPath = path.join(mdDir, file);
-      fs.readFile(mdPath, 'utf-8', (err, md) => {
-        if (err) {
-          throw new Error('Failed to read md file');
-        }
-        req.filename = file;
-        req.contents = metaMarked(md);
-        next();
-      });
+function listUpPost(req, res, next) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(mdDir, (err, mdFiles) => {
+      if (err) {
+        throw new Error('Failed to read mdDir');
+      }
+      const postList = [];
+      for (let i = 0; i < mdFiles.length; i++) {
+        fs.readFile(mdDir + mdFiles[i], 'utf-8', (err, md) => {
+          if (err) {
+            throw new Error('Failed to read md file: ' + mdFiles[i]);
+          }
+          const postTitle = md.match(/^\#\s(.)+\n/)[0].match(/[^#\n\s]+/);
+          const postDescription = md.match(/\n\>(.)+\n/)[0].match(/[^>\n\s]+/);
+          postList.push({
+            title: postTitle[0],
+            description: postDescription[0],
+            url: mdFiles[i]
+          });
+          console.log(postList);
+          req.postList = postList;
+        });
+      }
     });
   });
+  next();
 }
 
-app.get('/', readMd, (req, res) => {
-  res.send({
-    postList: {
-      title: req.contents.meta.title,
-      url: req.filename
-    }
-    // head.title
-    // head.description
-    // head.url
-    // head.fbimg
-    // head.twimg
-    // head.twaccount
-  });
+function readMd(mdPath) {
+  return function (req, res, next) {
+    fs.readFile(mdPath, 'utf-8', (err, md) => {
+      if (err) {
+        throw new Error('Failed to read md file: ' + mdPath);
+      }
+      req.contents = marked(md);
+      next();
+    });
+  };
+}
+
+app.get('/', listUpPost, (req, res) => {
+  // listUpPost().then(postList => {
+  //   // req.postList = postList;
+  //   console.log(postList);
+  //   res.send(req.postList);
+  // });
+  res.send(req.postList);
+  // res.render('index', {
+  //   locals: {
+  //     postList: {
+  //       title: req.contents.meta.title,
+  //       url: req.filename
+  //     }
+  //     // head.title
+  //     // head.description
+  //     // head.url
+  //     // head.fbimg
+  //     // head.twimg
+  //     // head.twaccount
+  //   }
+  //
+  // });
 });
 
 marked.setOptions({
@@ -65,9 +94,11 @@ app.get('/:post.md', (req, res) => {
     name: req.params.post,
     ext: '.md'
   });
+
+  // res.send(req.contents.html);
   fs.readFile(file, 'utf-8', (err, md) => {
     if (err) {
-      throw new Error('Failed to read md file');
+      throw new Error('Failed to read md: ' + file);
     }
     marked(md, (err, html) => {
       if (err) {
